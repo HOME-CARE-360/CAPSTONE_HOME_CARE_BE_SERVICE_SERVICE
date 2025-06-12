4ac0edb6b9d09175d6be9cd2e308da2be524efa2
tientran1234 <tranngoctien29112003@gmail.com>
2025-06-12 17:44:10 +0700

    edit micro

--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -4,9 +4,6 @@ import { HashingService } from './services/hashing.service';
 import { TokenService } from './services/token.service';
 import { JwtModule } from '@nestjs/jwt';
 import { AIService } from './services/AI.services';
-import { AccessTokenGuard } from './guards/access-token.guard';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthenticationGuard } from './guards/authentication.guard';
 import { PaymentAPIKeyGuard } from './guards/api-key.guard';
 import { EmailService } from './services/email.service';
 import { SharedUserRepository } from './repositories/shared-user.repo';
@@ -39,12 +36,7 @@ const sharedServices = [
 @Global()
 @Module({
   providers: [
-    ...sharedServices,
-    AccessTokenGuard,
-    {
-      provide: APP_GUARD,
-      useClass: AuthenticationGuard,
-    },
+    ...sharedServices
   ],
   exports: sharedServices,
   imports: [JwtModule],
