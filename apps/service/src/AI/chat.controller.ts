c2affbd8b3d093f2d42fe1b27010569c7f5684cb
tientran1234 <tranngoctien29112003@gmail.com>
2025-08-29 21:19:59 +0700

    add controller

--- apps/service/src/AI/chat.controller.ts
+++ apps/service/src/AI/chat.controller.ts
@@ -4,7 +4,9 @@ import { ChatService } from 'libs/common/src/services/chatAI.service';
 import {
     ChatRequest,
 } from 'libs/common/src/request-response-type/chatAI/chat.model';
+import { Controller } from '@nestjs/common';
 
+@Controller("chat")
 export class ChatMsController {
     constructor(private readonly chat: ChatService) { }
 
